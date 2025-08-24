4e9cc3698d8b9c4239b6ee40cbc41c5756f5a059
Volodymyr Rozdobudko <volodymyrr@nayax.com>
2025-08-24 20:30:29 +0300

    fixed

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,10 +2,12 @@ package com.example.demo.repository;
 
 import com.example.demo.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
 }
