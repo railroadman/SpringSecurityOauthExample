25b405b1384911b4a67574aafe25625b572a583e
Vladimir <railroad.man2k@gmail.com>
2025-08-24 18:11:22 +0300

    Save authenticated users

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -2,27 +2,28 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.nayax.erp.user.AuthService;
 
 @Configuration
 public class SecurityConfig {
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity http, AuthService authService) throws Exception {
         http
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/", "/css/**", "/js/**", "/images/**").permitAll()
                 .anyRequest().authenticated()
             )
-                .oauth2Login(o -> o
-                        .defaultSuccessUrl("/", true))
+            .oauth2Login(o -> o
+                .userInfoEndpoint(u -> u.userService(authService))
+                .defaultSuccessUrl("/", true))
             .logout(logout -> logout
                 .logoutSuccessUrl("/")
                 .invalidateHttpSession(true)
                 .clearAuthentication(true)
-                .deleteCookies("JSESSIONID")
-            );
+                .deleteCookies("JSESSIONID"));
         return http.build();
     }
 }
