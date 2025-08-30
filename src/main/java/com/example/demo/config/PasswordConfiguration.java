05647c923cb3e66b7d13efeea688307d11f137ce
Volodymyr Rozdobudko <volodymyrr@nayax.com>
2025-08-30 17:03:57 +0300

    some changes

--- src/main/java/com/example/demo/config/PasswordConfiguration.java
+++ src/main/java/com/example/demo/config/PasswordConfiguration.java
@@ -1,9 +1,11 @@
 package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 public class PasswordConfiguration {
 
     @Bean
